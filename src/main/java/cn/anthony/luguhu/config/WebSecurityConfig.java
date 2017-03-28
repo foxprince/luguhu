9ffe953d74ee51e5e3afc34d1879551c6001a9ed
foxprince <9600348@qq.com>
2017-03-28 21:28:52 +0800

    嗷嗷

--- src/main/java/cn/anthony/luguhu/config/WebSecurityConfig.java
+++ src/main/java/cn/anthony/luguhu/config/WebSecurityConfig.java
@@ -1,16 +1,13 @@
 package cn.anthony.luguhu.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+//@Configuration
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
