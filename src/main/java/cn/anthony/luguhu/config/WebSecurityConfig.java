ef1ea629b025a6e1e2b1ea12723119cdecef4f7b
foxprince <9600348@qq.com>
2017-04-06 10:12:05 +0800

    那你呢

--- src/main/java/cn/anthony/luguhu/config/WebSecurityConfig.java
+++ src/main/java/cn/anthony/luguhu/config/WebSecurityConfig.java
@@ -17,25 +17,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Build the request matcher for CSFR protection
-		//http://blog.netgloo.com/2014/09/28/spring-boot-enable-the-csrf-check-selectively-only-for-some-requests/
+		// http://blog.netgloo.com/2014/09/28/spring-boot-enable-the-csrf-check-selectively-only-for-some-requests/
 		RequestMatcher csrfRequestMatcher = new RequestMatcher() {
 			// Disable CSFR protection on the following urls:
-			private AntPathRequestMatcher[] requestMatchers = {new AntPathRequestMatcher("/wp/**") };
+			private AntPathRequestMatcher[] requestMatchers = { new AntPathRequestMatcher("/wp/**") };
 
 			@Override
 			public boolean matches(HttpServletRequest request) {
-				// If the request match one url the CSFR protection will bedisabled
+				// If the request match one url the CSFR protection will be disabled
 				for (AntPathRequestMatcher rm : requestMatchers) {
 					if (rm.matches(request)) {
 						return false;
 					}
 				}
 				return true;
-			} // method matches
-		}; // new RequestMatcher
+			} 
+		}; 
 		// Set security configurations
-		http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and().authorizeRequests()
-				.antMatchers("/**", "/resources/**", "/api/**", "/wp/**").permitAll();
+		// http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and()
+		http.csrf().ignoringAntMatchers("/wp/**").and().authorizeRequests().antMatchers("/**", "/resources/**", "/api/**", "/wp/**")
+				.permitAll();
 		// .antMatchers("/users/**").hasAuthority("ADMIN").anyRequest().fullyAuthenticated().and().formLogin()
 		// .loginPage("/login").failureUrl("/login?error").usernameParameter("email").permitAll().and().logout()
 		// .logoutUrl("/logout").deleteCookies("remember-me").logoutSuccessUrl("/").permitAll().and().rememberMe();
--- src/main/java/cn/anthony/luguhu/wp/WechatController.java
+++ src/main/java/cn/anthony/luguhu/wp/WechatController.java
@@ -1,5 +1,9 @@
 package cn.anthony.luguhu.wp;
 
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpMessageHandler;
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutTextMessage;
 
 /**
  * @author Binary Wang
@@ -28,7 +37,45 @@ public class WechatController {
 	@Autowired
 	private WxMpMessageRouter router;
 
-	
+	/**
+     * 初始化路由过滤规则
+     */
+    @PostConstruct 
+    public void init() {
+      router
+//          .rule()
+//              .async(false)
+//              .msgType(WxConsts.XML_MSG_EVENT).event(WxConsts.EVT_SUBSCRIBE) // 微信推送过来的消息的类型，和发送给微信xml格式消息的消息类型
+//              .handler(subscribeHandler)
+//              .end()
+//          .rule()
+//              .async(false)
+//              .msgType(WxConsts.XML_MSG_EVENT).event(WxConsts.EVT_UNSUBSCRIBE)
+//              .handler(unsubscribeHandler)
+//              .end()
+//          .rule()
+//              .async(false)
+//              .msgType(WxConsts.XML_MSG_EVENT).event(WxConsts.EVT_VIEW)
+//              .handler(viewHandler)
+//              .end()
+//          .rule()
+//              .async(false)
+//              .msgType(WxConsts.XML_MSG_EVENT).event(WxConsts.EVT_CLICK)
+//              .handler(clickHandler)
+//              .end()
+          .rule()
+              .async(false)
+              .msgType(WxConsts.XML_MSG_TEXT)
+              .handler(new WxMpMessageHandler(){
+					@Override
+					public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService,
+							WxSessionManager sessionManager) throws WxErrorException {
+						WxMpXmlOutTextMessage out = new WxMpXmlOutTextMessage();
+						out.setContent("echo:"+wxMessage.getContent());
+						return out;
+					}})
+              .end();
+    } 
 	@GetMapping(produces = "text/plain;charset=utf-8")
 	public String authGet(@RequestParam(name = "signature", required = false) String signature,
 			@RequestParam(name = "timestamp", required = false) String timestamp,
@@ -68,23 +115,39 @@ public class WechatController {
 			// aes加密的消息
 			WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(requestBody, this.wxService.getWxMpConfigStorage(), timestamp,
 					nonce, msgSignature);
-			this.logger.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
+			this.logger.info("\n消息解密后内容为：\n{} ", inMessage.toString());
 			WxMpXmlOutMessage outMessage = this.route(inMessage);
 			if (outMessage == null) {
 				return "";
 			}
 			out = outMessage.toEncryptedXml(this.wxService.getWxMpConfigStorage());
 		}
-		this.logger.debug("\n组装回复信息：{}", out);
+		this.logger.info("\n组装回复信息：{}", out);
 		return out;
 	}
 
 	private WxMpXmlOutMessage route(WxMpXmlMessage message) {
 		try {
+//			WxMpMessageInterceptor interceptor;
+//			WxMpMessageHandler handler;
+//			router.rule().msgType("MSG_TYPE").event("EVENT").eventKey("EVENT_KEY").content("CONTENT").interceptor(interceptor)
+//					.handler(handler).end().rule()
+//					// 另外一个匹配规则
+//					.end();
 			return this.router.route(message);
 		} catch (Exception e) {
 			this.logger.error(e.getMessage(), e);
 		}
 		return null;
 	}
+}
+class SubscribeHandle implements WxMpMessageHandler {
+
+	@Override
+	public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService,
+			WxSessionManager sessionManager) throws WxErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
