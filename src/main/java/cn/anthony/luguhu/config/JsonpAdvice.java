d075a0b3c091dc6538572682207cb1c9315235a9
foxprince <9600348@qq.com>
2018-02-24 11:52:05 +0800

    ddd

--- src/main/java/cn/anthony/luguhu/config/JsonpAdvice.java
+++ src/main/java/cn/anthony/luguhu/config/JsonpAdvice.java
@@ -3,7 +3,7 @@ package cn.anthony.luguhu.config;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
-@ControllerAdvice(basePackages = "cn.anthony.luguhu")
+@ControllerAdvice
 public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
 
     public JsonpAdvice() {
