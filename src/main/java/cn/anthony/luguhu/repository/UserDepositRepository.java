db5285255d1886cb758d91f947653e838dc377e3
foxprince <9600348@qq.com>
2018-02-26 23:20:17 +0800

    d

--- src/main/java/cn/anthony/luguhu/repository/UserDepositRepository.java
+++ src/main/java/cn/anthony/luguhu/repository/UserDepositRepository.java
@@ -1,7 +1,14 @@
 package cn.anthony.luguhu.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import cn.anthony.luguhu.domain.QUserDeposit;
+import cn.anthony.luguhu.domain.User;
 import cn.anthony.luguhu.domain.UserDeposit;
 public interface UserDepositRepository extends BaseRepository<UserDeposit, QUserDeposit, Long> {
-
+	@RestResource
+	Page<UserDeposit> findByUser(@Param("user") User user, Pageable pageable);
 }
